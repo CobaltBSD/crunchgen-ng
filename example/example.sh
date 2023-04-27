fb4a40fd65c457d517c28ca34e0aa395b90c01f7
CobaltBSD <sleed@posteo.cl>
2023-04-26 18:59:28 -0700

    Improved example

--- example/example.sh
+++ example/example.sh
@@ -1,13 +1,12 @@
 case "${1}" in
 	build)
 		../crunchgen -m Makefile example.conf 2>/dev/null
-		make -C src
 		make
 		;;
 	clean)
 		make clean
-		make -C src clean
 		rm -fv example.cache example.c Makefile
+		find -name "*.o" -delete
 		;;
 	*)
 		echo "Valid options: [build|clean]"
